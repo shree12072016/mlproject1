798d2456747c0f56e9b675664c35d6c5f8657086
Dhanawade Deepak Bapurao -X (ddhanawa - PRODAPT NMS INDIA PRIVATE LIMITED at Cisco) <ddhanawa@cisco.com>
2023-06-21 10:21:33 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages,setup
+
+from typing import List
+
+hypen_e = "-e ."
+
+def get_requirements(file_path:str)->list[str]:
+
+    requirements = []
+
+    with open(file_path) as file_obj:
+
+        requirements = file_obj.readlines()
+
+        requirements= [req.replace("\n","") for req in requirements]
+
+        if hypen_e in requirements:
+
+            requirements.remove(hypen_e)
+
+    return requirements
+
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Deepak',
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
+
+
+
