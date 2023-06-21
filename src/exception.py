1fd3f43566a118d22e0ea7f86552d88a3e9ccc94
Dhanawade Deepak Bapurao -X (ddhanawa - PRODAPT NMS INDIA PRIVATE LIMITED at Cisco) <ddhanawa@cisco.com>
2023-06-21 13:25:39 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb   = error_detail.exc_info()
@@ -22,3 +23,5 @@ class CustomException(Exception):
 
         return self.error_message
     
+
+
